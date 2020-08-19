b83518e0589a50c66b57a1ea5a9b162063543f20
Ghazale <behboud@uvic.ca>
2020-08-19 13:42:12 -0700

    Updated to lower resources

--- gpu_single_node_job.sh
+++ gpu_single_node_job.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-agullive
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100:8
-#SBATCH --exclusive
-#SBATCH --cpus-per-task=28
-#SBATCH --mem=150G
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=12G
 #SBATCH --time=1-00:00
 module load arch/avx512 StdEnv/2018.3
 source /home/behboud/ENV/bin/activate
